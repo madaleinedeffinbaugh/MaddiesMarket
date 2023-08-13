a21819626ab034be1283d4b15ba64a50b714c1cb
Madaleine Deffinbaugh <madaleine.deffinbaugh@gmail.com>
2023-08-12 21:16:36 -0600

    ad categories model

---
+++ src/main/java/com/maddiesmarket/springadlister/models/AdCategories.java
@@ -0,0 +1,36 @@
+package com.maddiesmarket.springadlister.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+
+@Entity
+@Table(name = "spring_ads_categories")
+public class AdCategories {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "ad_id")
+    private Ad ad;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    public AdCategories(Ad ad, Category category) {
+        this.ad = ad;
+        this.category = category;
+    }
+}
