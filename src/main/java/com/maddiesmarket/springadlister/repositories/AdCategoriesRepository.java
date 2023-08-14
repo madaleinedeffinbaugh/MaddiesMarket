db17ac673bd338b90509fc7ac040d186564c9e36
Madaleine Deffinbaugh <madaleine.deffinbaugh@gmail.com>
2023-08-13 22:16:24 -0600

    update ad categories repository

--- src/main/java/com/maddiesmarket/springadlister/repositories/AdCategoriesRepository.java
+++ src/main/java/com/maddiesmarket/springadlister/repositories/AdCategoriesRepository.java
@@ -3,6 +3,9 @@ package com.maddiesmarket.springadlister.repositories;
 import com.maddiesmarket.springadlister.models.AdCategories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AdCategoriesRepository extends JpaRepository<AdCategories, Long> {
+    Optional<AdCategories> findByAdIdAndCategoryId(Long adId, Long categoryId);
 
 }
