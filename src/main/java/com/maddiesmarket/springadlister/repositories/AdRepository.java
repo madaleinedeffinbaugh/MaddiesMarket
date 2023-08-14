045ae0364dde0245af920fd7bc76ac886564efa0
Madaleine Deffinbaugh <madaleine.deffinbaugh@gmail.com>
2023-08-13 22:16:45 -0600

    update ad repository

--- src/main/java/com/maddiesmarket/springadlister/repositories/AdRepository.java
+++ src/main/java/com/maddiesmarket/springadlister/repositories/AdRepository.java
@@ -3,6 +3,9 @@ package com.maddiesmarket.springadlister.repositories;
 import com.maddiesmarket.springadlister.models.Ad;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AdRepository extends JpaRepository<Ad, Long> {
+    List<Ad> findByArchivedFalse();
 
 }
