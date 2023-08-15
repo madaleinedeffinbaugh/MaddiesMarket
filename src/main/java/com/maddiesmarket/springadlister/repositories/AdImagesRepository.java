463696c53f6fbb7ddcecc96ff6e6193160fd9c66
Madaleine Deffinbaugh <madaleine.deffinbaugh@gmail.com>
2023-08-14 20:37:36 -0600

    updated ad images repository

--- src/main/java/com/maddiesmarket/springadlister/repositories/AdImagesRepository.java
+++ src/main/java/com/maddiesmarket/springadlister/repositories/AdImagesRepository.java
@@ -1,12 +1,9 @@
 package com.maddiesmarket.springadlister.repositories;
 
-import com.maddiesmarket.springadlister.models.AdCategories;
 import com.maddiesmarket.springadlister.models.AdImages;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 
 public interface AdImagesRepository extends JpaRepository<AdImages, Long> {
-    Optional<AdImages> findByAdIdAndUrl(Long adId, String url);
 
 }
